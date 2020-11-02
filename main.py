5ed12d2d5216a063e85cedc824fc254558a269c0
Jonah Broyer <broyerj@oregonstate.edu>
2020-11-01 17:42:24 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+"""
+    Re-implement the quick sort and use an insertion sort on lists that
+    have a small length ("partition limit").
+    Perform an analysis using different list sizes for the partition limit.
+"""
