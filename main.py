8362cd2deb96b33db92044ea684f0d8cc1b0110f
Jonah Broyer <broyerj@oregonstate.edu>
2020-11-18 10:06:06 -0800

    implemented quicksort with insertion sort

--- main.py
+++ main.py
@@ -3,3 +3,72 @@
     have a small length ("partition limit").
     Perform an analysis using different list sizes for the partition limit.
 """
+
+
+def quickSort(alist):
+    quickSortHelper(alist, 0, len(alist) - 1)
+
+
+def quickSortHelper(alist, first, last):
+    if first < last:
+        splitpoint = partition(alist, first, last)
+
+        quickSortHelper(alist, first, splitpoint - 1)
+        quickSortHelper(alist, splitpoint + 1, last)
+
+
+def partition(alist, first, last):
+    pivotvalue = alist[first]
+
+    leftmark = first + 1
+    rightmark = last
+
+    done = False
+    while not done:
+
+        while leftmark <= rightmark and alist[leftmark] <= pivotvalue:
+            leftmark = leftmark + 1
+
+        while alist[rightmark] >= pivotvalue and rightmark >= leftmark:
+            rightmark = rightmark - 1
+
+        if rightmark < leftmark:
+            done = True
+        else:
+            temp = alist[leftmark]
+            alist[leftmark] = alist[rightmark]
+            alist[rightmark] = temp
+
+    temp = alist[first]
+    alist[first] = alist[rightmark]
+    alist[rightmark] = temp
+
+    return rightmark
+
+
+def insertionSort(alist):
+    for index in range(1, len(alist)):
+
+        currentvalue = alist[index]
+        position = index
+
+        while position > 0 and alist[position - 1] > currentvalue:
+            alist[position] = alist[position - 1]
+            position = position - 1
+
+        alist[position] = currentvalue
+
+
+def main(alist):
+    if len(alist) <= 5:
+        insertionSort(alist)
+        print(alist)
+    else:
+        quickSort(alist)
+        print(alist)
+
+
+if __name__ == '__main__':
+    # alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    alist = [54, 26, 93, 17, 77]
+    main(alist)
